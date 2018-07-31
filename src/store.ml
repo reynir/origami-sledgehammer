6f89607758c15ca9fde5a4850c04ae2f17a166d0
Reynir Björnsson <reynir@reynir.dk>
2018-07-31 11:33:53 +0200

    Fix Store semantics
    New indices will be properly wrapped when they are larger than max.

--- src/store.ml
+++ src/store.ml
@@ -7,16 +7,17 @@ module Make(Max : sig val max : int end) : S =
 struct
   open Max
 
-  let counter = ref (-1)
+  let counter = ref (-1L)
   let alloc () =
-    incr counter; !counter mod max
+    Int64.(counter := rem (add !counter 1L) (of_int max));
+    !counter
 
   let store : string option array = Array.make max None
 
   let put s : int64 =
-    let idx = alloc () mod max in
-    store.(idx) <- Some s;
-    Int64.of_int idx
+    let idx = alloc () in
+    store.(Int64.to_int idx) <- Some s;
+    idx
 
   let get idx : string option =
     let idx = Int64.to_int idx in
